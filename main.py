41dfe126eb50925b34ab80d0866eb7b085a6417a
Martins <martins.frolovs@gmail.com>
2019-07-09 22:58:46 +0300

    Improvements

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from contrastive_predictive_coding.imagenet_dataset import get_imagenet_datasets
 from torch.utils.data import DataLoader
 
 import itertools
+import random
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-
+DEVICE = 'cpu'
 
 class ResnetEncoder(Module):
     def __init__(self):
@@ -79,15 +81,14 @@ class ResnetEncoder(Module):
     def forward(self, x):
 
         x = self.prep.forward(x)
-        print(f'shape after prep {x.shape}')
+        #print(f'shape after prep {x.shape}')
         for i in range(self.num_blocks):
             x = self.resnet_blocks[i].forward(x)
+            #print(f'shape after resnet_block {i} {x.shape}')
 
-            print(f'shape after resnet_block {i} {x.shape}')
-
-        print(f'shape after resnet {x.shape}')
+        #print(f'shape after resnet {x.shape}')
         x = self.avg_pool.forward(x)
-        print(f'shape after avg_pool {x.shape}')
+        ##print(f'shape after avg_pool {x.shape}')
 
         return x
 
@@ -157,15 +158,15 @@ class ContextPredictionModel(Module):
                 x2 = x1 + 2
 
                 context_input = x[:,:,y1:y2+1,x1:x2+1]
-                print(f"x1:{x1} y1:{y1}, x2:{x2}, y2:{y2}")
+                #print(f"x1:{x1} y1:{y1}, x2:{x2}, y2:{y2}")
 
-                print(f"context_input shape {context_input.shape}")
+                #print(f"context_input shape {context_input.shape}")
                 context = self.context_conv.forward(context_input)
 
-                print(f"context shape {context.shape}")
+                #print(f"context shape {context.shape}")
 
                 context = context.squeeze()
-                print(f"context shape {context.shape}")
+                #print(f"context shape {context.shape}")
 
                 # UP - 0, RIGHT = 1, DOWN = 2, LEFT = 3
 
@@ -176,10 +177,10 @@ class ContextPredictionModel(Module):
                     if y3 > 6:
                         break
 
-                    print(f"pred for y3:{y3} x1+1:{x1+1}")
+                    #print(f"pred for y3:{y3} x1+1:{x1+1}")
 
                     prediction = self.prediction_weights[0][steps_up].forward(context)
-                    print(f"predfiction shape on context {prediction.shape}")
+                    #print(f"predfiction shape on context {prediction.shape}")
                     prediction_list.append(
                         dict(
                             y=y3,
@@ -194,9 +195,9 @@ class ContextPredictionModel(Module):
                     if x3 > 6:
                         break
 
-                    print(f"pred for y1+1:{y1+1} x3:{x3}")
+                    #print(f"pred for y1+1:{y1+1} x3:{x3}")
                     prediction = self.prediction_weights[1][steps_right].forward(context)
-                    print(f"predfiction shape on context {prediction.shape}")
+                    #print(f"predfiction shape on context {prediction.shape}")
                     prediction_list.append(
                         dict(
                             y=y1+1,
@@ -220,20 +221,59 @@ class ClassificationModel(Module):
 
 
 NUM_CLASSES = 100
-BATCH_SIZE = 5
+BATCH_SIZE = 2
 data_path = "/Users/martinsf/ai/deep_learning_projects/data/imagenet_images"
 dataset_train, dataset_test = get_imagenet_datasets(data_path, num_classes = NUM_CLASSES)
+
+NUM_RANDOM_PATCHES = 50
+random_patch_loader = DataLoader(dataset_train, NUM_RANDOM_PATCHES, shuffle=True)
+
+def get_random_patches():
+
+    global random_patch_loader
+
+    try:
+        img_batch = next(iter(random_patch_loader))['image']
+    except StopIteration:
+        random_patch_loader = DataLoader(dataset_train, NUM_RANDOM_PATCHES, shuffle=True)
+
+    if len(img_batch) < NUM_RANDOM_PATCHES:
+        random_patch_loader = DataLoader(dataset_train, NUM_RANDOM_PATCHES, shuffle=True)
+        img_batch = next(iter(random_patch_loader))['image']
+
+
+    patches = []
+
+    for i in range(NUM_RANDOM_PATCHES):
+        x = random.randint(0,6)
+        y = random.randint(0,6)
+
+        patches.append(img_batch[i:i+1,:,x*32:x*32+64,y*32:y*32+64])
+
+        # plt.imshow(np.transpose(patches[-1][0],(1,2,0)))
+        # plt.show()
+
+    patches_tensor = torch.cat(patches, dim=0)
+    return patches_tensor
+
+
 
 data_loader_train = DataLoader(dataset_train, BATCH_SIZE, shuffle = True)
 data_loader_test = DataLoader(dataset_test, BATCH_SIZE, shuffle = True)
 
-resnet_encoder = ResnetEncoder()
-context_predictor_model = ContextPredictionModel(in_channels=1024)
+resnet_encoder = ResnetEncoder().to(DEVICE)
+context_predictor_model = ContextPredictionModel(in_channels=1024).to(DEVICE)
 
 optimizer = torch.optim.Adam(params = itertools.chain(resnet_encoder.parameters(), context_predictor_model.parameters()), lr=0.0001)
 
+def cos_loss(a,b):
+    ret = torch.exp(torch.sum(a*b, dim=1))
+    return ret
+
+torch.autograd.set_detect_anomaly(True)
+
 for batch in data_loader_train:
-    img_arr = batch['image'][0]
+
 
     # plt.imshow(img_arr.permute(1,2,0))
     # fig, axes = plt.subplots(7,7)
@@ -244,15 +284,13 @@ for batch in data_loader_train:
     for y_patch in range(7):
         for x_patch in range(7):
 
-            img_patches = batch['image'][0]
-
             y1 = y_patch * 32
             y2 = y1 + 64
 
             x1 = x_patch * 32
             x2 = x1 + 64
 
-            img_patches = batch['image'][:,:,y1:y2,x1:x2] # Batch(img_idx in batch), channels xrange, yrange
+            img_patches = batch['image'].to(DEVICE)[:,:,y1:y2,x1:x2] # Batch(img_idx in batch), channels xrange, yrange
             img_patches = img_patches.unsqueeze(dim=1)
             all_patches_list.append(img_patches)
 
@@ -265,13 +303,16 @@ for batch in data_loader_train:
 
     patch_batch = torch.cat(patches_per_image, dim = 0)
 
-
     patches_encoded = resnet_encoder.forward(patch_batch)
     patches_encoded = patches_encoded.squeeze()
     patches_encoded = patches_encoded.view(BATCH_SIZE, 7,7,-1)
     patches_encoded = patches_encoded.permute(0,3,1,2)
 
-    print(f"shape after reshape{patches_encoded.shape}")
+    random_patches = get_random_patches().to(DEVICE)
+    enc_random_patches = resnet_encoder.forward(random_patches).detach()
+    enc_random_patches = enc_random_patches.squeeze()
+
+    #print(f"shape after reshape{patches_encoded.shape}")
 
     predictions = context_predictor_model.forward(patches_encoded)
 
@@ -282,16 +323,23 @@ for batch in data_loader_train:
         target = patches_encoded[:,:,p['y'],p['x']]
         pred = p['prediction']
 
-        print(f"target shape {target.shape} pred shape {pred.shape}")
+        #print(f"target shape {target.shape} pred shape {pred.shape}")
 
-        losses.append((pred-target)/ BATCH_SIZE)
+        good_term = cos_loss(pred, target)
+        divisor = cos_loss(pred, target)
+
+        for random_patch_idx in range(NUM_RANDOM_PATCHES):
+            divisor = divisor + cos_loss(pred, enc_random_patches[random_patch_idx:random_patch_idx+1])
+            # divisor += cos_loss(pred, enc_random_patches[random_patch_idx:random_patch_idx+1])
+
+        losses.append(-torch.log(good_term/divisor))
 
     loss = torch.sum(torch.cat(losses))
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
 
-    print(loss.detach().to('cpu'))
+    print(f"Loss: {loss.detach().to('cpu')}")
 
 
 #Training the encoder network with the prediction task
