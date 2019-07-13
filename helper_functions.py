92cb80b2e1819e7fa725eec94b4572978bbb8d6d
Martins <martins.frolovs@gmail.com>
2019-07-13 22:08:14 +0300

    hacky precisioin tuning fix

--- helper_functions.py
+++ helper_functions.py
@@ -114,7 +114,7 @@ def write_csv_stats(csv_path, stats_dict):
     for key, value in stats_dict.items():
         if isinstance(value, float):
             precision = 0.001
-            stats_dict[key] =  ((value * precision ) // 1.0 ) / precision
+            stats_dict[key] =  ((value / precision ) // 1.0 ) * precision
 
     with open(csv_path, "a") as f:
         csv_writer = csv.writer(f)
